402c47080fec222de541c970a2579a7146160661
amao <amao@ALVINM>
2017-05-28 00:00:25 -0500

    Totoal Order

---
+++ OrderTrackng.BusinessObjects/Editable/UserLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderTrackng.BusinessObjects
+{
+    public partial class UserLogin
+    {
+        public int TotalOrders
+        {
+            get
+            {
+                ShipBobEntities1 sp = new ShipBobEntities1();
+                return sp.OrderDetails.Where(o => o.UserID == this.UserID).Count();
+            }
+        }
+    }
+}
